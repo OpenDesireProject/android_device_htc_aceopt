8fd9f4396d7e65d15c1246887dab4335d5ea6147
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2014-01-26 12:46:18 -0800

    reorganize idc, kcm and kl files
    Change-Id: I3ab71591632b964aad87e2689662eeda5a3f40bd

--- device.mk
+++ device.mk
@@ -85,21 +85,21 @@ PRODUCT_COPY_FILES += \
 
 # Keylayouts and keychars
 PRODUCT_COPY_FILES += \
-	$(COMMON_PATH)/keylayout/keychars/atmel-touchscreen.kcm:system/usr/keychars/atmel-touchscreen.kcm \
-	$(COMMON_PATH)/keylayout/keychars/synaptics-rmi-touchscreen.kcm:system/usr/keychars/synaptics-rmi-touchscreen.kcm \
-	$(COMMON_PATH)/keylayout/keychars/elan-touchscreen.kcm:system/usr/keychars/elan-touchscreen.kcm \
-	$(COMMON_PATH)/keylayout/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
-	$(COMMON_PATH)/keylayout/keylayout/spade-keypad.kl:system/usr/keylayout/spade-keypad.kl \
-	$(COMMON_PATH)/keylayout/keylayout/atmel-touchscreen.kl:system/usr/keylayout/atmel-touchscreen.kl \
-	$(COMMON_PATH)/keylayout/keylayout/synaptics-rmi-touchscreen.kl:system/usr/keylayout/synaptics-rmi-touchscreen.kl \
-	$(COMMON_PATH)/keylayout/keylayout/elan-touchscreen.kl:system/usr/keylayout/elan-touchscreen.kl
+	$(COMMON_PATH)/rootdir/system/usr/keychars/atmel-touchscreen.kcm:system/usr/keychars/atmel-touchscreen.kcm \
+	$(COMMON_PATH)/rootdir/system/usr/keychars/synaptics-rmi-touchscreen.kcm:system/usr/keychars/synaptics-rmi-touchscreen.kcm \
+	$(COMMON_PATH)/rootdir/system/usr/keychars/elan-touchscreen.kcm:system/usr/keychars/elan-touchscreen.kcm \
+	$(COMMON_PATH)/rootdir/system/usr/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+	$(COMMON_PATH)/rootdir/system/usr/keylayout/spade-keypad.kl:system/usr/keylayout/spade-keypad.kl \
+	$(COMMON_PATH)/rootdir/system/usr/keylayout/atmel-touchscreen.kl:system/usr/keylayout/atmel-touchscreen.kl \
+	$(COMMON_PATH)/rootdir/system/usr/keylayout/synaptics-rmi-touchscreen.kl:system/usr/keylayout/synaptics-rmi-touchscreen.kl \
+	$(COMMON_PATH)/rootdir/system/usr/keylayout/elan-touchscreen.kl:system/usr/keylayout/elan-touchscreen.kl
 
 # Input device config
 PRODUCT_COPY_FILES += \
-	$(COMMON_PATH)/idc/atmel-touchscreen.idc:system/usr/idc/atmel-touchscreen.idc \
-	$(COMMON_PATH)/idc/spade-keypad.idc:system/usr/idc/spade-keypad.idc \
-	$(COMMON_PATH)/idc/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc \
-	$(COMMON_PATH)/idc/elan-touchscreen.idc:system/usr/idc/elan-touchscreen.idc
+	$(COMMON_PATH)/rootdir/system/usr/idc/atmel-touchscreen.idc:system/usr/idc/atmel-touchscreen.idc \
+	$(COMMON_PATH)/rootdir/system/usr/idc/spade-keypad.idc:system/usr/idc/spade-keypad.idc \
+	$(COMMON_PATH)/rootdir/system/usr/idc/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc \
+	$(COMMON_PATH)/rootdir/system/usr/idc/elan-touchscreen.idc:system/usr/idc/elan-touchscreen.idc
 
 # Copy bcm4329 firmware
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4329/device-bcm.mk)
