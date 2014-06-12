cca19c7a6a873bdb206f15ad85f7a53513c61cd1
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2014-06-12 15:34:17 -0700

    use cache partition for system app dexopt
    Change-Id: I749a10ba5afc84033a4037522a4613529aa48cd4

--- device.mk
+++ device.mk
@@ -131,6 +131,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.vold.umsdirtyratio=50
 
+# Use cache partition for system app dexopt
+PRODUCT_PROPERTY_OVERRIDES += \
+	dalvik.vm.dexopt-data-only=0
+
 # Set build date
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
