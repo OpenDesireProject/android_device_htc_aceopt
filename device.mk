702cb8ec2f1152b76ac6797fc01d820fa93e12d7
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2014-11-05 16:31:37 -0800

    build ProximityCalibrator
    Change-Id: Iaeb74fea40ef8f577179d5632a12697c2e28eb29

--- device.mk
+++ device.mk
@@ -118,6 +118,10 @@ PRODUCT_PACKAGES += \
 	sensors.spade \
 	librpc
 
+# Proximity Recalibrator
+PRODUCT_PACKAGES += \
+	ProximityRecalibrator
+
 # Permissions
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
