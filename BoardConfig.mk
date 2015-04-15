36b59ae75b9e28e11016098e0e6e4f02c970505c
jrior001 <jriordan001@gmail.com>
2015-04-14 23:32:18 -0400

    TWRP updates
    Change-Id: I78918ac99e4bdcd9aad75b1ba5c05643a2ff2ebf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,6 +59,7 @@ TARGET_KERNEL_CONFIG := ace_defconfig
 
 TARGET_RECOVERY_FSTAB := device/htc/msm7x30-common/rootdir/fstab.htc7x30
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+BOARD_HAS_NO_SELECT_BUTTON := true
 
 # Brightness
 BRIGHTNESS_SYS_FILE := /sys/devices/platform/leds-pm8058/leds/keyboard-backlight/brightness
@@ -75,18 +76,16 @@ BOARD_BLUEDROID_VENDOR_CONF := device/htc/ace/bluetooth/libbt_vndcfg.txt
 # Spade DSP profile
 COMMON_GLOBAL_CFLAGS += -DWITH_SPADE_DSP_PROFILE
 
-# Charge mode
-BOARD_CHARGING_MODE_BOOTING_LPM := /sys/htc_lpm/lpm_mode
-
 # TWRP
-DEVICE_RESOLUTION := 480x800
+TW_THEME := portrait_mdpi
 TW_FLASH_FROM_STORAGE := true
 TW_INTERNAL_STORAGE_PATH := "/sdcard"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "sdcard"
-TW_INCLUDE_JB_CRYPTO := true
+TW_INCLUDE_CRYPTO := true
 TW_NO_SCREEN_BLANK := true
 TW_INCLUDE_DUMLOCK := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 #RECOVERY_VARIANT := twrp
-TW_NO_CPU_TEMP := true
 TW_NO_USB_STORAGE := true
+TW_NO_CPU_TEMP := true
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/htc_lpm/lpm_mode
