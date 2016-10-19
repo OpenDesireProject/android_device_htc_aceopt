OpenDesireProject/android_device_htc_sagaopt

Fix Bluetooth (Thanks to bastei)
Use pyramid firmware and config
See msm7x30-common for part2

BoardConfig.mk
@@ -69,8 +69,9 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storag
 BOARD_VOLD_MAX_PARTITIONS := 36
 
 # Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/sagaopt/bluetooth
-BOARD_BLUEDROID_VENDOR_CONF := device/htc/sagaopt/bluetooth/libbt_vndcfg.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/sagaopt/bluetooth/include
+BOARD_BLUEDROID_VENDOR_CONF := device/htc/sagaopt/bluetooth/vnd_saga.txt
+BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY := false
 
 # Camera
 BOARD_HAVE_HTC_FFC := true

bluetooth/include/bdroid_buildcfg.h
@@ -1,0 +1,10 @@
+#ifndef _BDROID_BUILDCFG_H
+#define _BDROID_BUILDCFG_H
+#define BTM_DEF_LOCAL_NAME "HTC Desire S"
+#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+#define I2SPCM_SLAVE_BRCM TRUE
+#define BLE_INCLUDED FALSE
+#define BTA_GATT_INCLUDED FALSE
+#define SMP_INCLUDED FALSE
+#define REMOVE_EAGER_THREADS FALSE
+#endif

OpenDesireProject/android_device_htc_aceopt

BoardConfig.mk
@@ -80,8 +80,9 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storag
 BOARD_VOLD_MAX_PARTITIONS := 36
 
 # Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/aceopt/bluetooth
-BOARD_BLUEDROID_VENDOR_CONF := device/htc/aceopt/bluetooth/libbt_vndcfg.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/aceopt/bluetooth/include
+BOARD_BLUEDROID_VENDOR_CONF := device/htc/aceopt/bluetooth/vnd_ace.txt
+BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY := false
 
 # Spade DSP profile
 COMMON_GLOBAL_CFLAGS += -DWITH_SPADE_DSP_PROFILE

bluetooth/include/bdroid_buildcfg.h
@@ -1,0 +1,10 @@
+#ifndef _BDROID_BUILDCFG_H
+#define _BDROID_BUILDCFG_H
+#define BTM_DEF_LOCAL_NAME "HTC Desire HD"
+#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+#define I2SPCM_SLAVE_BRCM TRUE
+#define BLE_INCLUDED FALSE
+#define BTA_GATT_INCLUDED FALSE
+#define SMP_INCLUDED FALSE
+#define REMOVE_EAGER_THREADS FALSE
+#endif
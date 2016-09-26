3e39f92229d79ba096109b710277d638acf5c18b
bastei <bastei@nocontact>
2016-09-26 21:38:58 +0200

    Fix Bluetooth (Thanks to bastei)
    Use pyramid firmware and config
    See msm7x30-common for part2

--- ace.mk
+++ ace.mk
@@ -85,16 +85,8 @@ PRODUCT_COPY_FILES += \
 	$(COMMON_PATH)/rootdir/system/usr/idc/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc \
 	$(COMMON_PATH)/rootdir/system/usr/idc/elan-touchscreen.idc:system/usr/idc/elan-touchscreen.idc
 
-# Copy bcm4329 firmware
-$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4329/device-bcm.mk)
-
-# BCM4329 firmware
-PRODUCT_COPY_FILES += \
-	$(COMMON_PATH)/rootdir/system/vendor/firmware/bcm4329.hcd:system/vendor/firmware/bcm4329.hcd
-
-# BT / Lights / Sensors
+# Lights / Sensors
 PRODUCT_PACKAGES += \
-	libbt-vendor \
 	lights.spade \
 	sensors.spade
 
